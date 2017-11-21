8ce05b0e7f736765bdf2a9ed264a47f76f8506e3
Brian Mwathi <mwathibrian7@gmail.com>
2017-11-21 22:31:19 +0300

    Added charts to dashboard

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -8,15 +8,43 @@ import { TweetService } from '../../core/tweet.service';
 export class DashboardComponent implements OnInit {
 
   summary: any[];
+  // barChart
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels: string[] = ['positive', 'negative', 'neutral'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData: any[] = [
+    {data: [1,2,3], label: 'Series A'}
+  ];
+  
+
+  // Pie
+  public pieChartLabels: string[] = ['positive', 'negative', 'neutral'];
+  public pieChartData: number[] = [11,12,13];
+  public pieChartType = 'pie';
+
 
   constructor(private _tweetService: TweetService) { }
+  
+    ngOnInit() {
+      this._tweetService.getTweetSummary().subscribe(
+        data => {
+          this.summary = data;
+          this.barChartData = [{data: [data[1].total_tweets, data[0].total_tweets, data[2].total_tweets], label: 'Series A'}];
+          this.pieChartData = [data[1].total_tweets, data[0].total_tweets, data[2].total_tweets];
+        });
+    }
 
-  ngOnInit() {
-    this._tweetService.getTweetSummary().subscribe(
-      data => {
-        this.summary = data;
-        console.log(data);
-      }
-    )
+  // events
+  public chartClicked(e: any): void {
+    console.log(e.active[0]._model);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
   }
 }
